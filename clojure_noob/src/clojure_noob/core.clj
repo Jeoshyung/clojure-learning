7d46a481aa582f13a7c3aae5401f899c65e3a4b2
Jeoshyung <ininjakia@gmail.com>
2025-04-01 21:49:05 +0800

    Experimenting with if and do statements

--- clojure_noob/src/clojure_noob/core.clj
+++ clojure_noob/src/clojure_noob/core.clj
@@ -4,4 +4,19 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+
+  (def value
+  (+ 1 2 3 4 5 6 7 8 9 10)
+  )
+
+  (println "The value is: " value)
+
+  (if (> value 10)
+    (do
+      (println "Value is greater than 10")
+      (println "The bool is: " (> value 10)))
+    (do
+    (println "Value is less than or equal to 10")
+    (println "The bool is: " (> value 10)))
+  )
+)
