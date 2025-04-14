ace36b104695e732315386b196438292e991b4eb
Jeoshyung <ininjakia@gmail.com>
2025-04-14 18:59:02 +0800

    Refactor operations into functions

--- clojure_noob/src/clojure_noob/core.clj
+++ clojure_noob/src/clojure_noob/core.clj
@@ -1,6 +1,17 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(defn add [a b]
+  (+ a b))
+(defn subtract [a b]
+  (- a b))
+(defn multiply [a b]
+  (* a b))
+(defn divide [a b]
+  (if (not= b 0)
+    (/ a b)
+    (throw (IllegalArgumentException. "Cannot divide by zero"))))
+
 (defn -main
   "Simple Calculator App"
   [& args]
@@ -17,10 +28,10 @@
         (println "Enter second number:")
         (let [num2 (Integer/parseInt(read-line))]
           (cond
-            (= operation "1") (println "Result: " (+ num1 num2))
-            (= operation "2") (println "Result: " (- num1 num2))
-            (= operation "3") (println "Result: " (* num1 num2))
-            (= operation "4") (println "Result: " (/ num1 num2))
+            (= operation "1") (println "Result: " (add num1 num2))
+            (= operation "2") (println "Result: " (subtract num1 num2))
+            (= operation "3") (println "Result: " (multiply num1 num2))
+            (= operation "4") (println "Result: " (divide num1 num2))
             :else (println "Invalid operation")
           )
         )
